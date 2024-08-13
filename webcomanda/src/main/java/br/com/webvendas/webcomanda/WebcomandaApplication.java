57ff775c05a3d4781e8937c7a1dfdd5fa4b61062
dbengp <dbengp@hotmail.com>
2024-08-13 03:06:59 -0300

    first commit

---
+++ webcomanda/src/main/java/br/com/webvendas/webcomanda/WebcomandaApplication.java
@@ -0,0 +1,13 @@
+package br.com.webvendas.webcomanda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebcomandaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebcomandaApplication.class, args);
+	}
+
+}
