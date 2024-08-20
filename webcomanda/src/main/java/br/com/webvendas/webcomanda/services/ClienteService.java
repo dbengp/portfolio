994172cc9198c77270acdf707d4dd159fd4e0d46
dbengp <dbengp@hotmail.com>
2024-08-20 00:39:32 -0300

    alterado exceção personalizada do update do recurso clientes

--- webcomanda/src/main/java/br/com/webvendas/webcomanda/services/ClienteService.java
+++ webcomanda/src/main/java/br/com/webvendas/webcomanda/services/ClienteService.java
@@ -7,6 +7,7 @@ import java.util.Optional;
 
 import br.com.webvendas.webcomanda.exceptions.ExcecaoDeBancoDeDados;
 import br.com.webvendas.webcomanda.exceptions.ExcecaoDeRecursoNaoEncontrado;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
