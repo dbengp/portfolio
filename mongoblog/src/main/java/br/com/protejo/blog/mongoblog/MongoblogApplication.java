5e86ac1f990f5f8d2495f69082cff90681ffb70a
dbengp <dbengp@hotmail.com>
2024-08-20 00:42:23 -0300

    inicio do projeto de blog com mongodb

---
+++ mongoblog/src/main/java/br/com/protejo/blog/mongoblog/MongoblogApplication.java
@@ -0,0 +1,13 @@
+package br.com.protejo.blog.mongoblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongoblogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongoblogApplication.class, args);
+	}
+
+}
---
+++ mongoblog/src/test/java/br/com/protejo/blog/mongoblog/MongoblogApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.protejo.blog.mongoblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongoblogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
